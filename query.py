e9d1e5c1839fae3c2dec9438f0649230fe76dd8e
Justinder Mahal <jmahal@emich.edu>
2023-11-04 01:38:20 +0000

    feature: shows running menu with user option to quit
    The first version of the menu shows one option, where the user
    can quit by inputting the character "q". If anything else is
    typed in at this point, the menu keeps reappearing prompting
    the user for input.
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,12 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+while True:
+
+    print("1. Press q to quit program")
+
+    option = input()
+
+    if(option == "q"):
+        break

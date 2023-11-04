9af2a90bb0cc891efbccdbc302f79392b7668c0f
Justinder Mahal <jmahal@emich.edu>
2023-11-04 03:33:55 +0000

    feature: search for cast member in all the movies
    This feature prompts the user for another input upon selection to
    provide a cast member, and if that name of the cast member is in
    any of the movies, it will return the name of the movie and the
    cast member
    
    issue #7

--- query.py
+++ query.py
@@ -7,6 +7,7 @@ while True:
     print("1. Press q to quit program")
     print("2. Press 2 to list all movies")
     print("3. Press 3 to search for movie title")
+    print("4. Press 4 to search for cast member")
 
     option = input()
 
@@ -20,4 +21,10 @@ while True:
         for film in movies._movies:
             if(title.lower() in film['name'].lower()):      # lower() makes case insensitve
                 print(film['name'])
-            
+    elif(option == "4"):
+        person = input(" Enter the name of a cast member: ")
+        for film in movies._movies:
+            for index in range(len(film['cast'])):          # each film has different list length for cast values
+                if (person.lower() in film['cast'][index].lower()):      
+                    print(film['name'])
+                    print(film['cast'][index])      

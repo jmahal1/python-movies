8290b154b1cf5d42459e9bcd5d565c83d007678d
Justinder Mahal <jmahal@emich.edu>
2023-11-04 02:45:23 +0000

    feature: user option to list all the movies
    using a for loop and a print statement for the name key of
    each dictionary of list, the user can choose option "2" and
    see a printout of all the movies accessible via the menu
    
    issue #3

--- query.py
+++ query.py
@@ -5,8 +5,12 @@ movies = Movies('./movies.txt')
 while True:
 
     print("1. Press q to quit program")
+    print("2. Press 2 to list all movies")
 
     option = input()
 
     if(option == "q"):
         break
+    elif(option == "2"):
+        for film in movies._movies:
+            print(film['name'])
